578d0ca9ce1bc435d73552e01316d48e9402c94d
Laljith123 <lalvictory7@gmail.com>
2026-03-28 23:06:17 +0530

    Add music.py to load and display JSON data

---
+++ music.py
@@ -0,0 +1,15 @@
+import os
+import csv
+from yt_dlp import YoutubeDL
+from pathlib import Path
+import json
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+OUTPUT_JSON = PROJECT_ROOT / "output.json"
+
+print("Loading output.json...")
+with open(OUTPUT_JSON, "r") as f:
+    data = json.load(f)
+    print("Loaded data from output.json:")
+    print(data)
